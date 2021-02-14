afd918d50051f6a5a71911c5e75c3ea20fe62c6e
toribricker <tori.bricker@icloud.com>
2021-02-14 13:31:34 -0600

    finished

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,60 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using TechJobsPersistent.Models;
-using TechJobsPersistent.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using TechJobsPersistent.Data;
+using TechJobsPersistent.Models;
+using TechJobsPersistent.ViewModels;
+public class EmployerController : Controller
 
 
-
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace TechJobsPersistent.Controllers
 {
-    public class EmployerController : Controller
+    // GET: /<controller>/
+    private JobDbContext context;
+
+    public EmployerController(JobDbContext dbContext)
     {
-        private JobDbContext context;
+        context = dbContext;
+    }
 
-        public EmployerController(JobDbContext dbContext)
-        {
-            context = dbContext;
-        }
+    public IActionResult Index()
+    {
+       
+        List<Employer> employers = context.Employers.ToList();
+        return View(employers);
+    }
 
-        public IActionResult Index()
-        {
-            List<Employer> employers = context.Employers.ToList();
-            return View(employers);
-        }
+    public IActionResult Add()
+    {
+        
+        Employer employer = new Employer();
+        return View(employer);
+    }
 
-        public IActionResult Add()
-        {
-            Employer employer = new Employer();
-            return View(employer);
-        }
-
+  
         [HttpPost]
-        public IActionResult AddEmployer(Employer employer)
+    public IActionResult AddEmployer(Employer employer)
+    {
+       
+        if (ModelState.IsValid)
         {
-            if (ModelState.IsValid)
-            {
-                context.Employers.Add(employer);
-                context.SaveChanges();
-                return Redirect("/Employer/");
-            }
-            return View("Add", employer);
+            context.Employers.Add(employer);
+            context.SaveChanges();
+            return Redirect("/Employer/");
         }
+        return View("Add", employer);
+    }
 
-        public IActionResult About(int id)
-        {
-            List<Employer> jobEmployers = context.Employers
-                .Where(Employer => Employer.Id == id)
-               .Include(Employer => Employer.Name)
-               .Include(Employer => Employer.Location)
-               .ToList();
+    public IActionResult About(int id)
+    {
+       
+        List<Employer> jobEmployers = context.Employers
+                        .Where(Employer => Employer.Id == id)
+                       .Include(Employer => Employer.Name)
+                       .Include(Employer => Employer.Location)
+                       .ToList();
 
-            return View(jobEmployers);
-        }
+        return View(jobEmployers);
     }
 }
+ 
